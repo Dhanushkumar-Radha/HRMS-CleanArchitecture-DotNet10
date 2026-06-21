05391d116d2a8eff7af0b0adeec60a121e6a2fe2
Dhanushkumar-Radha <210701051@rajalakshmi.edu.in>
2026-06-21 18:54:26 +0530

    Setup AutoMapper

---
+++ HRLeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HRLeaveManagement.Application
+{
+    public  class ApplicationServicesRegistration
+    {
+        public ApplicationServicesRegistration(IServiceCollection services)
+        {
+         //services.AddAutoMapper(typeof(ApplicationServicesRegistration));
+         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+          
+        }
+    }
+}
---
+++ HRLeaveManagement.Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,17 @@
+﻿using HRLeaveManagement.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Application.DTOs
+{
+    public class LeaveAllocationDto : BaseDto
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto? LeaveType { get; set; }
+        //public Employee Employee { get; set; }
+        public string? EmployeeId { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
